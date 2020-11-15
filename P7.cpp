8770aea8538e8c7f08db096f0b4c4e9e0c6fc4f4
Raj Garg <39689983+me17b016@users.noreply.github.com>
2020-11-15 21:33:50 +0530

    Update P7.cpp

--- P7.cpp
+++ P7.cpp
@@ -1,37 +1,63 @@
 #include<iostream>
 #include<string>
-#include<random>
-#include<chrono>
+#include<time.h>
 #include<mpi.h>
 #include<omp.h>
-
 using namespace std;
 
 int main(int argc, char** argv) {
 	int rank, np;
-	//MPI_Init(NULL, NULL);
-	int provided;
-    int iproc;
-    int nproc;
-    int i;
-    int j;
-    int nthreads;
-    int tid;
-	MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
-	MPI_Comm_size(MPI_COMM_WORLD, &iproc);
-	MPI_Comm_rank(MPI_COMM_WORLD, &nproc);
-#pragma omp parallel private( tid )
-    {
-        tid = omp_get_thread_num();
-        nthreads = omp_get_num_threads();
-        for (i = 0; i <= nproc - 1; i++) {
-            MPI_Barrier(MPI_COMM_WORLD);
-            for (j = 0; j <= nthreads - 1; j++) {
-                if ((i == iproc) && (j == tid)) {
-                    cout << "MPI rank: " << iproc << " with thread ID: " << tid << endl;
-                }
-            }
-        }
-    }
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &np);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	int N = stoi(argv[1]);
+	int local_sum = 0, global_sum = 0;
+
+	if (!rank) {
+		int* arr;
+		arr = (int*)malloc(N * sizeof(int));
+		srand(time(0));
+		int su = 0;
+		for (int i = 0; i < N; i++) arr[i] = rand() % 100;
+		
+		int st = 0, en = 0, r = N % np;
+		en = (N / np);
+		if (r) en++;
+			
+		#pragma omp parallel for reduction(+: local_sum) 
+		for (int i = 0; i < en; i++) {
+			local_sum += arr[i];
+		}
+
+		for (int i = 1; i < np; i++) {
+			st = en, en += N / np;
+			if (r >= i + 1) en++;
+			int* tem; tem = (int*)malloc((en - st + 1) * sizeof(int));
+			tem[0] = en - st;
+			for (int j = st; j < en; j++) tem[j - st + 1] = arr[j];
+			MPI_Send(tem, (en - st + 1), MPI_INT, i, 0, MPI_COMM_WORLD);
+		}
+
+		MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		cout << "SUM is " << global_sum << '\n';
+	}
+	else {
+		int n = 1 + N / np;
+		int r = N % np;
+		if (r && r >= rank + 1) n++;
+
+		int* A; A = (int*)malloc((n) * sizeof(int));
+		MPI_Recv(A, n, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+		#pragma omp parallel for reduction(+: local_sum) 
+		for (int i = 1; i < n; i++) {
+			local_sum += A[i];
+		}
+		
+		MPI_Send(&local_sum, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Reduce(&local_sum, &global_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	}
+
 	MPI_Finalize();
 }
