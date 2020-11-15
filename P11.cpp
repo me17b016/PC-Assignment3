746014919c160a98cda9f7c21a4e6b2a10bd5d7d
Raj Garg <39689983+me17b016@users.noreply.github.com>
2020-11-15 23:46:31 +0530

    Create P11.cpp

---
+++ P11.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<time.h>
+#include<mpi.h>
+#include<thread>
+#include <mutex>
+using namespace std;
+std::mutex mtx;
+
+const int nthreads = 4;
+int cnt = 0;
+
+void iittp_barrier(int np, int rank, int tid) {
+	if (!tid) {
+		int logP = ceil(log(np) / log(2));
+		int i = 0;
+		int val;
+		for (i = 0; i < logP; ++i) {
+			int send_to = (rank + (int)pow(2, i)) % np;
+			int recv_from = rank - (int)pow(2, i);
+			while (recv_from < 0) {
+				recv_from += np;
+			}
+			
+			MPI_Send(&i, 1, MPI_INT, send_to, 0, MPI_COMM_WORLD);
+			MPI_Recv(&val, 1, MPI_INT, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+	}
+	mtx.lock();
+	cnt++;
+	mtx.unlock();
+	while ((cnt % nthreads) != 0);
+}
+
+void print(int tid, int rank, int np) {
+	cout << rank << ' ' << tid << '\n';
+	iittp_barrier(np, rank, tid);
+}
+
+int main(int argc, char** argv) {
+	int rank, np;
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &np);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	vector<thread> threads;
+
+	for (int tid = 0; tid < nthreads; tid++) {
+		threads.push_back(thread(print, tid, rank, np));
+	}
+	for (auto& th : threads) {
+		th.join();
+	}
+    MPI_Finalize();
+}
